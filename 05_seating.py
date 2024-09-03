d8a334a0b8729e0282cc1a76d57e900c33fd9554
EithanCapella <60627671+EithanCapella@users.noreply.github.com>
2024-09-03 02:09:01 -0400

    Update 05_seating.py

--- 05_seating.py
+++ 05_seating.py
@@ -1,12 +1,40 @@
-def seating_arrangements(sd: List[int], md: int) -> int:
-  result = 0
-  """
+from itertools import product
+from typing import List
 
-  YOUR CODE GOES HERE
+def seating_arrangements(sd: List[int], td: int) -> int:
+    nSeats = len(sd) + 1
+    arrangements = list(product([0, 1], repeat=nSeats))  # All seating arrangements
+    valid_count = 0
+    #print(arrangements)
+    
+    for arrangement in arrangements:
+        occupied_seats = []  #Store the indices of occupied seats
+        for i, seat in enumerate(arrangement): #gives us the seat value and index
+            if seat == 1:  #If the seat is taken
+                occupied_seats.append(i)
+        #print(occupied_seats)
+        
+        if len(occupied_seats) <= 1: #No need to check saids it is a valid seating
+            valid_count += 1
+            print(arrangement)
+        
+        elif len(occupied_seats) > 1:
+            # Calculate the sum of spaces between the first and next occupied seat found
+            for j in range(1, len(occupied_seats)):
+                # Calculate the space between consecutive occupied seats
+                start = occupied_seats[j-1]
+                end = occupied_seats[j]
+                space_between = 0
+            
+            # Check if the seating arrangement is valid
+            if space_between >= td:
+                valid_count += 1
+                print(arrangement)
+    
 
-  """
-  return result
 
+    print(valid_count)    
+    return valid_count
 
 #
 # TESTS
